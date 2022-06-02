d97aa8b9f45018fc477069c795e9359f2f8f7165
mattg44 <92577785+mattg44@users.noreply.github.com>
2022-06-02 14:00:38 +0100

    02062022

--- Projet_10/api/api_st.py
+++ Projet_10/api/api_st.py
@@ -1,9 +1,34 @@
 import streamlit as st
-import pickle
 import pandas as pd
 import numpy as np
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
 
-# def main():
+
+df = pd.read_csv("billets.csv", sep=";")
+
+# Définition du jeu de données
+X_pip = df.drop("is_genuine", axis=1)
+y_pip = df["is_genuine"]
+X_pip_train, X_pip_test, y_pip_train, y_pip_test = train_test_split(X_pip, y_pip, random_state=3, test_size=0.20)
+
+# Création du pipeline
+rf_pip = Pipeline(
+    steps=[
+        ("imputer", IterativeImputer()),
+        ("classifier", RandomForestClassifier(
+            criterion="gini",
+            max_depth=4,
+            max_features="auto",
+            n_estimators=100))
+        ])
+
+rf_pip.fit(X_pip_train, y_pip_train)
+
+
 st.title('Bank notes analysis')
 st.write("Select a csv file with the measures of bank notes to be checked.")
 
@@ -13,8 +38,6 @@ if uploaded_file is not None:
                     "FileSize": uploaded_file.size}
     st.write(file_details)
 
-# Load model
-model = pickle.load(open("/Projet_10/api/model.pkl", "rb"))
 
 # File manipulations
 if uploaded_file is not None:
@@ -27,10 +50,10 @@ if uploaded_file is not None:
         df_verif.drop("id", axis=1, inplace=True)
 
         # Checking file
-        df11 = pd.DataFrame(model.predict(df_verif))
+        df11 = pd.DataFrame(rf_pip.predict(df_verif))
         df11.columns = ["is_genuine"]
         df11["is_genuine"] = np.where(df11["is_genuine"] == 1, True, False)
-        df12 = pd.DataFrame(model.predict_proba(df_verif))
+        df12 = pd.DataFrame(rf_pip.predict_proba(df_verif))
         df12.columns = ["proba_false (%)", "proba_true (%)"]
         df12["proba_false (%)"] = round(df12["proba_false (%)"] * 100, 4)
         df12["proba_true (%)"] = round(df12["proba_true (%)"] * 100, 4)
@@ -44,6 +67,4 @@ if uploaded_file is not None:
         st.write(df_mask)
 
 
-# if __name__ == '__main__':
-#     main()
 
