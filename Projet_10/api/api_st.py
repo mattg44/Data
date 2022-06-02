4f02fd856a6aa06ed1eb69a88630e9ad07727378
mattg44 <92577785+mattg44@users.noreply.github.com>
2022-06-02 14:09:28 +0100

    Update api_st.py

--- Projet_10/api/api_st.py
+++ Projet_10/api/api_st.py
@@ -1,33 +1,20 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.experimental import enable_iterative_imputer
-from sklearn.impute import IterativeImputer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import Pipeline
 
 
 df = pd.read_csv("billets.csv", sep=";")
+df = df.dropna()
 
 # Définition du jeu de données
-X_pip = df.drop("is_genuine", axis=1)
-y_pip = df["is_genuine"]
-X_pip_train, X_pip_test, y_pip_train, y_pip_test = train_test_split(X_pip, y_pip, random_state=3, test_size=0.20)
+X = df.drop("is_genuine", axis=1)
+y = df["is_genuine"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=3, test_size=0.20)
 
-# Création du pipeline
-rf_pip = Pipeline(
-    steps=[
-        ("imputer", IterativeImputer()),
-        ("classifier", RandomForestClassifier(
-            criterion="gini",
-            max_depth=4,
-            max_features="auto",
-            n_estimators=100))
-        ])
-
-rf_pip.fit(X_pip_train, y_pip_train)
-
+rf = RandomForestClassifier(criterion="gini", max_depth=4, max_features="auto", n_estimators=100)
+rf.fit(X_train, y_train)
 
 st.title('Bank notes analysis')
 st.write("Select a csv file with the measures of bank notes to be checked.")
@@ -50,10 +37,10 @@ if uploaded_file is not None:
         df_verif.drop("id", axis=1, inplace=True)
 
         # Checking file
-        df11 = pd.DataFrame(rf_pip.predict(df_verif))
+        df11 = pd.DataFrame(rf.predict(df_verif))
         df11.columns = ["is_genuine"]
         df11["is_genuine"] = np.where(df11["is_genuine"] == 1, True, False)
-        df12 = pd.DataFrame(rf_pip.predict_proba(df_verif))
+        df12 = pd.DataFrame(rf.predict_proba(df_verif))
         df12.columns = ["proba_false (%)", "proba_true (%)"]
         df12["proba_false (%)"] = round(df12["proba_false (%)"] * 100, 4)
         df12["proba_true (%)"] = round(df12["proba_true (%)"] * 100, 4)
